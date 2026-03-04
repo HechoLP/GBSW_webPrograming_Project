8f899f2794acb238a6ff2045ddcff0aaaccee2a4
HechoLP <lrang20101@naver.com>
2026-03-04 12:30:06 +0900

    Update ex01.js
    변수 b 선언, b의 값을 변경, 변수 b 출력.
    상수 변수의 값을 변경시 어떠한 에러가 나는지 또한 확인.

--- ch01/ex01.js
+++ ch01/ex01.js
@@ -1,2 +1,12 @@
 const a = 10; // 변수 선언
+let b = 20;
+
 console.log(a);
+
+b = 30;
+console.log(b);
+
+// a = 40;
+// console.log(a)
+// // TypeError: Assignment to constant variable.
+// // const는 상수이기 때문에 변경시 오류발생
