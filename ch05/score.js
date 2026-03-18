d8ff4077e912eb380fbb96a55ba20a5a58ca8479
HechoLP <lrang20101@naver.com>
2026-03-18 11:56:21 +0900

    Create score.js
    점수대별 학점 판별 로직 구현
    * 91~100점(A)부터 51~60점(F)까지 10점 단위 학점 부여 조건문 작성
    * 범위 외 입력값에 대한 "범위를 벗어났습니다" 예외 처리 추가
    * readline 모듈을 활용한 사용자 입력 및 결과 출력 로직 구현

---
+++ ch05/score.js
@@ -0,0 +1,30 @@
+import readline from "readline";
+
+const rl = readline.createInterface({
+    input : process.stdin,
+    output : process.stdout,
+});
+
+rl.question("점수를 입력하세요. : ", function(score){
+    /*
+        100 ~ 91 점 - A학점
+        81 ~ 90 점 - B작점
+        71 ~ 80 점 - C작점
+        61 ~ 70 점 - D작점
+        51 ~ 60 점 - F작점
+        그 외  - 범위를 벗어났습니다
+    */
+    if (score >= 91 && score <= 100) {
+        console.log("A학점")
+    } else if (score >= 81 && score <= 90) {
+        console.log("B학점")
+    } else if (score >= 71 && score <= 80) {
+        console.log("C학점")
+    } else if (score >= 61 && score <= 70) {
+        console.log("D학점")
+    } else if (score >= 51 && score <= 60) {
+        console.log("F학점")
+    } else {
+        console.log("범위를 벗어났습니다.")
+    }
+});
