3b6ac83f719abbe51166f78ece5f730cacca891c
HechoLP <lrang20101@naver.com>
2026-03-18 11:30:21 +0900

    Update elseifts.js
    중첩 조건문을 활용한 성적 판별 및 장학 대상 선정 로직 추가
    * 90점 이상인 경우 "장학대상"을 판별하는 중첩 if문 구현
    * 60점 이상 "합격", 60점 미만 "불합격" 처리 로직 세분화
    * 0~100점 범위를 벗어난 입력값에 대한 유효성 검사 유지

--- ch05/elseifts.js
+++ ch05/elseifts.js
@@ -8,12 +8,17 @@ const rl = readline.createInterface({
 
 rl.question("점수를 입력하세요. : ", function(score){
     /*  점수는 0~100점 사이 범위
+        90점 이상이면 장학대상
         60점 이상이면 합격
         60점 미만이면 불합격 */
 
     if (score >= 60 && score <= 100){
-        console.log("합격힙니다.");
-    } else if (score < 60 && scroe >= 0){
+        if (score >= 90){
+            console.log("장학대상입니다.")
+        } else {
+            console.log("합격입니다.")
+        }
+    } else if (score < 60 && score >= 0){
         console.log("불합격입니다.");
     } else {
         console.log("유효하지 않은 값입니다.")
