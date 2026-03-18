7fe87948b5ed0783e9ee006597e6c8d5f8d89f7c
HechoLP <lrang20101@naver.com>
2026-03-18 14:51:59 +0900

    Create ndan.js
    for 반복문을 활용한 구구단 출력 로직 구현
    * readline 모듈을 통해 사용자로부터 출력할 단(dan)을 입력받음
    * for문을 1부터 9까지 순회하며 템플릿 리터럴을 이용한 구구단 형식 출력
    * 입력받은 단수에 대해 i=1부터 i=9까지의 곱셈 결과 및 rl.close() 종료 처리

---
+++ ch06/ndan.js
@@ -0,0 +1,17 @@
+const readline = require("readline")
+
+const rl = readline.createInterface({
+    input : process.stdin,
+    output : process.stdout,
+});
+
+rl.question("단 수를 입력하세요. : ", function(dan){
+    /*
+        $dan 의 구구단 출력
+        $dan * 1 ~ $dan * 9까지 출력
+    */
+   for (let i = 1; i <= 9; i++) {
+    console.log(`${dan} * ${i} = ${dan * i}`);
+    }
+    rl.close();
+});
