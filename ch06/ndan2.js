0eaa70f8cff104e1731e1956bba8df1ef3270173
HechoLP <lrang20101@naver.com>
2026-03-25 11:08:36 +0900

    Update ndan2.js

--- ch06/ndan2.js
+++ ch06/ndan2.js
@@ -1,4 +1,5 @@
 for (let i = 2; i <= 9; i++) {
+    console.log(`-----${i}단-----`)
     for (let j = 1; j <= 9; j++) {
         console.log(`${i} * ${j} = ${i * j}`);
     }
@@ -8,6 +9,7 @@ console.log("-----------")
 
 let i = 2;
 while (i <= 9) {
+    console.log(`-----${i}단-----`)
     let j = 1;
     while (j <= 9) {
         console.log(`${i} * ${j} = ${i * j}`);
